b94309e1f097b4510cebbb4e0a45b5f9c9ae7229
shenyiling <jzx19951028yyjy@163.com>
2020-09-04 21:20:40 +0800

    feat: AutoUpdate service

--- app/src/main/java/com/eder/coolweather/WeatherActivity.java
+++ app/src/main/java/com/eder/coolweather/WeatherActivity.java
@@ -5,6 +5,7 @@ import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
@@ -24,6 +25,7 @@ import android.widget.Toast;
 import com.bumptech.glide.Glide;
 import com.eder.coolweather.gson.Forecast;
 import com.eder.coolweather.gson.Weather;
+import com.eder.coolweather.service.AutoUpdateService;
 import com.eder.coolweather.util.HttpUtil;
 import com.eder.coolweather.util.Utility;
 
@@ -220,6 +222,13 @@ public class WeatherActivity extends AppCompatActivity {
         carWashText.setText(catWash);
         sportText.setText(sport);
         weatherLayout.setVisibility(View.VISIBLE);
+
+        if (weather != null && "ok".equals(weather.status)) {
+            Intent intent = new Intent(this, AutoUpdateService.class);
+            startService(intent);
+        } else {
+            Toast.makeText(WeatherActivity.this, "获取天气信息失败", Toast.LENGTH_SHORT).show();
+        }
     }
 }
 
